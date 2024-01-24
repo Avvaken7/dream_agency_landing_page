1d747527870f0bd43ce8a4b37ff4a7e554e9ba0c
Slava <avvaken7@gmail.com>
2024-01-24 16:51:01 +0200

    add animation on a screen and start to responsive

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: 'css',
+      apply: 'build',
+      transform(css) {
+        return {
+          code: css,
+          map: null,
+          // Вказати кодування тут
+          encoding: 'utf-8'
+        };
+      },
+    },
+  ],
+});

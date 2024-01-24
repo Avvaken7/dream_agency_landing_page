18d9f310111d25a46081236205bde231a111fa49
Slava <avvaken7@gmail.com>
2024-01-24 17:15:00 +0200

    add animation on a screen and start to responsive

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import postcss from 'rollup-plugin-postcss'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import postcss from 'rollup-plugin-postcss';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    postcss({
-      config: './postcss.config.js',      
-    })
-  ],
+  plugins: [react(), postcss()],
 });

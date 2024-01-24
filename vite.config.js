b0d855abd0f87fc04772111186da954d234e3db0
Slava <avvaken7@gmail.com>
2024-01-24 20:33:08 +0200

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcss from 'rollup-plugin-postcss';
 
 export default defineConfig({
-  plugins: [react(), postcss()],
+  plugins: [react()],
 });
